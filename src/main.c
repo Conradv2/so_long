775e906228c3341bd3ba1c1bae7905625fa40264
Conrad <marek226@interia.pl>
2025-03-20 22:27:57 +0100

    getting there....

--- src/main.c
+++ src/main.c
@@ -11,30 +11,48 @@
 /* ************************************************************************** */
 
 #include "../minilibx-linux/mlx.h"
-#include "stdio.h"
-#include "stdlib.h"
 #include "so_long.h"
-#include "X11/keysym.h"
 
-int	ft_keysym(int keysym, t_game_window *window_ptr)
+int	ft_render_window(t_game_window *window_ptr)
 {
-	if (keysym == XK_Escape)
+	window_ptr->mlx_win = mlx_new_window(window_ptr->mlx,
+			1280, 720, "Siemaneczko!");
+	if (window_ptr->mlx_win == NULL)
 	{
-		printf("%d key (Esc) has been pressed\n", keysym);
-		mlx_destroy_window(window_ptr->mlx, window_ptr->mlx_win);
 		mlx_destroy_display(window_ptr->mlx);
 		free(window_ptr->mlx);
-		exit(1);
+		return (1);
 	}
-	if (keysym == XK_a)
-		mlx_string_put(window_ptr->mlx, window_ptr->mlx_win, 1280/2, 720/2, 0xFF0000, "KUUUURWA PIWO, kurwaaaaa kocham kondziaaaaa");
-	if (keysym == XK_d)
-		mlx_clear_window(window_ptr->mlx, window_ptr->mlx_win);
-	printf("%d key was pressed\n", keysym);
-	if (keysym == XK_s)
-		printf("TEST");
 	return (0);
 }
+
+void	ft_color_screen (t_game_window *window_ptr)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+	y = 0;
+	while (i < 721)
+	{
+		y = 0;
+		while (y < 1281)
+		{
+			mlx_pixel_put(window_ptr->mlx, window_ptr->mlx_win, y, i, 0xFF0000);
+			y++;
+		}
+		i++;
+	}
+	/*for (i = 0; i < 1921; i++)
+	{
+		for (y = 0; y < 1081; y++)
+		{
+			mlx_pixel_put(window_ptr->mlx, window_ptr->mlx_win, i, y, 0xFF0000);
+		}
+	}*/
+}
+
+void	ft_load_textures(void);
 
 int	main(void)
 {
@@ -43,15 +61,9 @@ int	main(void)
 	window.mlx = mlx_init();
 	if (window.mlx == NULL)
 		return (1);
-	window.mlx_win = mlx_new_window(window.mlx, 1280, 720, "Siemaneczko!");
-	if (window.mlx_win == NULL)
-	{
-		mlx_destroy_display(window.mlx);
-		free(window.mlx);
-		return (1);
-	}
-	ft_keysym(115, window.mlx_win);
+	ft_render_window(&window);
 	mlx_key_hook(window.mlx_win, ft_keysym, &window);
+	//ft_load_textures(void);
 	mlx_loop(window.mlx);
 	mlx_destroy_window(window.mlx, window.mlx_win);
 	mlx_destroy_display(window.mlx);
--- src/so_long.h
+++ src/so_long.h
@@ -13,10 +13,20 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include "../minilibx-linux/mlx.h"
+# include "stdlib.h"
+# include "X11/keysym.h"
+# include "stdio.h"
+
 typedef struct s_game_window
 {
 	void	*mlx;
 	void	*mlx_win;
 }	t_game_window;
 
+typedef struct s_textures
+{
+}	t_game_textures;
+int		ft_keysym(int keysym, t_game_window *window_ptr);
+void	ft_color_screen (t_game_window *window_ptr);
 #endif
