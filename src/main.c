bf15afdcbaabbcfc3a5907e6b487ed994ac88c0d
Conradv2 <marek226@interia.pl>
2025-03-22 20:35:15 +0100

    starting again

--- src/main.c
+++ src/main.c
@@ -3,51 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conrad <conrad@student.42.fr>              +#+  +:+       +#+        */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 19:04:00 by conrad            #+#    #+#             */
-/*   Updated: 2025/03/18 19:04:01 by conrad           ###   ########.fr       */
+/*   Created: 2025/03/22 20:34:15 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/22 20:34:18 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minilibx-linux/mlx.h"
-#include "so_long.h"
-
-void	ft_color_screen(t_game_window *window_ptr);
-void	ft_load_textures(void);
-
-int	main(void)
-{
-	t_game_window	window;
-
-	window.mlx = mlx_init();
-	if (window.mlx == NULL)
-		return (1);
-	ft_render_window(&window);
-	mlx_key_hook(window.mlx_win, ft_keysym, &window);
-	//ft_load_textures(void);
-	mlx_loop(window.mlx);
-	mlx_destroy_window(window.mlx, window.mlx_win);
-	mlx_destroy_display(window.mlx);
-	free(window.mlx);
-	return (0);
-}
-
-void	ft_color_screen(t_game_window *window_ptr)
-{
-	int	i;
-	int	y;
-
-	i = 0;
-	y = 0;
-	while (i < 721)
-	{
-		y = 0;
-		while (y < 1281)
-		{
-			mlx_pixel_put(window_ptr->mlx, window_ptr->mlx_win, y, i, 0xFF0000);
-			y++;
-		}
-		i++;
-	}
-}
--- src/so_long.h
+++ src/so_long.h
@@ -3,31 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conrad <conrad@student.42.fr>              +#+  +:+       +#+        */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 19:04:04 by conrad            #+#    #+#             */
-/*   Updated: 2025/03/18 19:04:04 by conrad           ###   ########.fr       */
+/*   Created: 2025/03/22 20:34:38 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/22 20:34:39 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-
-# include "../minilibx-linux/mlx.h"
-# include "stdlib.h"
-# include "X11/keysym.h"
-# include "stdio.h"
-
-typedef struct s_game_window
-{
-	void	*mlx;
-	void	*mlx_win;
-}	t_game_window;
-
-typedef struct s_textures
-{
-}	t_game_textures;
-int		ft_keysym(int keysym, t_game_window *window_ptr);
-int		ft_render_window(t_game_window *window_ptr);
-void	ft_color_screen(t_game_window *window_ptr);
-#endif
