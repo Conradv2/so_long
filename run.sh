7efdfd86e855570078b5ea94a3326d644930e8e5
Conradv2 <marek226@interia.pl>
2025-03-22 22:57:03 +0100

    working run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cc src/*.c -I minilibx-linux/ -L minilibx-linux/  -lmlx_Linux -lXext -lX11 -Wall -Werror -Wextra -g && ./a.out
+cc src/*.c -I minilibx-linux/ -I ft_printf/ -L minilibx-linux/ -L ft_printf/ -lftprintf -lmlx_Linux -lXext -lX11 -Wall -Werror -Wextra -g && ./a.out
