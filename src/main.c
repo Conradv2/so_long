15fc8bab92d939e63cb2b40bc22ea71164b32965
Conradv2 <marek226@interia.pl>
2025-03-22 23:20:31 +0100

    cleaner main.c

--- src/main.c
+++ src/main.c
@@ -14,15 +14,21 @@
 #include "../ft_printf/ft_printf.h"
 #include <fcntl.h>
 
+
+void	ft_game_loop(t_game *game)
+{
+	mlx_key_hook(game->mlx_win, ft_key_input, game);
+	mlx_loop(game->mlx);
+	mlx_destroy_window(game->mlx, game->mlx_win);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+}
+
 int	main(void)
 {
 	t_game	game;
 
 	ft_render_window(&game);
-	mlx_key_hook(game.mlx_win, ft_key_input, &game);
-	mlx_loop(game.mlx);
-	mlx_destroy_window(game.mlx, game.mlx_win);
-	mlx_destroy_display(game.mlx);
-	free(game.mlx);
+	ft_game_loop(&game);
 	return (0);
 }
