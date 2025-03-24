2cade3ad12a39f9dc467ca27dec7553c6a03dad4
Conradv2 <marek226@interia.pl>
2025-03-24 20:46:15 +0100

    added new function declarations

--- src/so_long.h
+++ src/so_long.h
@@ -18,19 +18,37 @@
 # include "X11/keysym.h"
 # include "stdio.h"
 # include "unistd.h"
-#include "get_next_line.h"
+# include "get_next_line.h"
+# include "../ft_printf/ft_printf.h"
+# include <fcntl.h>
 
 typedef struct s_texture
 {
 }	t_texture;
 
+typedef struct s_map
+{
+	char	**map_arr;
+	char	*line;// <--testing
+	int		row;
+	int		column;
+	int		fd_map;// <--testing
+	int		i;// <--testing
+}	t_map;
+
 typedef struct s_game
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_map	map;
 }	t_game;
 
 void	ft_render_window(t_game *window);
 void	ft_game_loop(t_game *game);
+char	**ft_create_arr(t_map *map); // <--this returns a pointer to a 2d array that i just create;
+char	**ft_fill_arr(void); // <--this fills created array with elements in a map.ber
+void	ft_print_arr(t_game *game); // <--not that neccesary at this point but nice to have for testing;
+void	ft_handle_map(t_game *game); // <-- this will handle map buff creation and filling;
+void	ft_free_arr(t_game *game); // <--this will free the memory of created array at the end of a program;
 int		ft_key_input(int keysym, t_game *game);
 #endif
