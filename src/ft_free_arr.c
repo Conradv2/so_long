0e28368b0318486eb0c1266670badd73d675d866
Conradv2 <marek226@interia.pl>
2025-03-24 23:25:11 +0100

    made the functions work with new ft_create_arr and ft_fill_arr

---
+++ src/ft_free_arr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free_arr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 22:27:32 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/24 22:27:33 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_free_arr(t_game *game)
+{
+	game->map.i = 0;
+
+	while (game->map.i < game->map.row)
+	{
+		free(game->map.map_arr[game->map.i]);
+		game->map.i++;
+	}
+	free(game->map.map_arr);
+}
---
+++ src/ft_handle_map.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_handle_map.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 20:43:07 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/24 20:43:08 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_handle_map(t_game *game)
+{
+
+	ft_create_arr (game);
+	ft_fill_arr(game);
+	// game->map.map_arr = ft_fill_arr(&map); <-- this later also
+	// ft_print_arr(game); //<--this later
+}
