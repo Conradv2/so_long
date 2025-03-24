fbc154902dc412f1b1c4734d74606f22d0017589
Conradv2 <marek226@interia.pl>
2025-03-24 20:47:16 +0100

    created new function that allocates memory for 2d array

---
+++ src/ft_create_arr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_arr.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 20:42:59 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/24 20:43:01 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+char	**ft_create_arr(t_map *map)
+{
+	map->i = 0;
+	map->row = 0;
+	map->fd_map = open("maps/map.ber", O_RDONLY);
+	if (map->fd_map < 0)
+	{
+		perror("Error while loading a map...");
+		exit(1);
+	}
+	while (1)
+	{
+		map->line = get_next_line(map->fd_map);
+		if (map->line == NULL)
+			break ;
+		map->column = ft_strlen(map->line);
+		map->row++;
+		free(map->line);
+	}
+	map->map_arr = (char **)malloc(map->row * sizeof(char *));
+	while (map->i < map->row)
+		map->map_arr[map->i++] = (char *)malloc(map->column * sizeof(char));
+	close(map->fd_map);
+	return (map->map_arr);
+}
+// i mean it works like i wanted it to work
