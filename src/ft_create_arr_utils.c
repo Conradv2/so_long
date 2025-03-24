8bc7bf9ad4fe39a17f0128025dd2b4deb6de2753
Conradv2 <marek226@interia.pl>
2025-03-24 21:53:06 +0100

    some useless functions for ft_create_arr.c to work

---
+++ src/ft_create_arr_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_so_long_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 21:00:34 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/24 21:00:35 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_get_col_row(t_map *map)
+{
+	while (1)
+	{
+		map->line = get_next_line(map->fd_map);
+		if (map->line == NULL)
+			break ;
+		map->column = ft_strlen(map->line);
+		map->row++;
+		free(map->line);
+	}
+}
+
+void	ft_malloc_check(t_map *map)
+{
+	int	j;
+
+	j = 0;
+	if (map->map_arr[map->i] == NULL)
+	{
+		while (j++ < map->i)
+			free(map->map_arr[j]);
+		free (map->map_arr);
+		exit (1);
+	}
+}
---
+++ src/ft_create_arr_utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_arr_utils.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 21:03:59 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/24 21:04:02 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CREATE_ARR_UTILS_H
+# define FT_CREATE_ARR_UTILS_H
+# include "so_long.h"
+
+void	ft_get_col_row(t_map *map);
+void	ft_malloc_check(t_map *map);
+
+#endif
