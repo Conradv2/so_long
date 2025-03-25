21ea9ea6674b3f43840fe27bce1edad00196c30e
Conradv2 <marek226@interia.pl>
2025-03-25 21:03:13 +0100

    added open() func check

--- src/ft_fill_arr.c
+++ src/ft_fill_arr.c
@@ -17,7 +17,12 @@ void	ft_fill_arr(t_game *game)
 	int	j;
 
 	j = 0;
-	game->map.fd_map = open("maps/map.ber", O_RDONLY);
+	game->map.fd_map = open("map.ber", O_RDONLY);
+	if (game->map.fd_map < 0)
+	{
+		perror("ft_fill_array: file doesn't exist");
+		exit(1);
+	}
 	game->map.i = 0;
 	while (1)
 	{
