1e0c1934980e71ec53f2564b2f05a958cd313b57
Conradv2 <marek226@interia.pl>
2025-03-26 23:06:24 +0100

    created new function that exits a program in a clean way and added it to the ft_key_input func

--- src/ft_exit_game.c
+++ src/ft_exit_game.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_key_input.c                                     :+:      :+:    :+:   */
+/*   ft_exit_game.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/22 22:51:47 by conradv2          #+#    #+#             */
-/*   Updated: 2025/03/22 22:51:48 by conradv2         ###   ########.fr       */
+/*   Created: 2025/03/26 23:04:48 by conradv2          #+#    #+#             */
+/*   Updated: 2025/03/26 23:04:48 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include "../ft_printf/ft_printf.h"
 
-int	ft_key_input(int keysym, t_game *game)
+int	ft_exit_game(t_game *game)
 {
-	ft_printf("%d is being pressed\n", keysym);
-	if (keysym == XK_Escape)
-	{
-		ft_printf("Pressed Esc, closing game!\n");
-		ft_exit_game(game);
-	}
-	if (keysym == XK_a)
-	{
-		ft_printf("TEST\n");
-	}
-	return (0);
+	ft_free_arr(game);
+	mlx_destroy_window(game->mlx, game->mlx_win);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+	exit(0);
 }
