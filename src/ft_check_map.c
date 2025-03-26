6d51d05fe0cc90c92bc0a55bad4bd67dcdfa1536
Conradv2 <marek226@interia.pl>
2025-03-27 00:57:28 +0100

    new function, need to take look at this one, i think it should work on created array to not get confused

--- src/ft_check_map.c
+++ src/ft_check_map.c
@@ -12,12 +12,44 @@
 
 #include "ft_create_arr_utils.h"
 #include "so_long.h"
-#include <fcntl.h>
 
 void	ft_check_map(t_game *game)
 {
-	game->map.i = 0;
-	game->map.row = 0;
+	ft_printf("Welcome to the rice fields\n");
+	char		tmp;
+	int			j;
+	int			i;
+
+	j = 0;
+	i = 0;
+	tmp = '1';
 	ft_map_init(&game->map);
+	ft_get_col_row(game);
+	close(game->map.fd_map);
+	ft_map_init(&game->map);
+	while (1)
+	{
+		game->map.line = get_next_line(game->map.fd_map);
+		if (game->map.line == NULL)
+		{
+			ft_printf("again gnl fail");
+			break ;
+		}	
+		ft_printf("%s", game->map.line);
+		j = 0;
+		while (j < game->map.row)
+		{
+			if (tmp != game->map.line[j] && i < game->map.column)
+			{
+				ft_printf("Man, this is bullshit...");
+				free(game->map.line);
+				close(game->map.fd_map);
+				exit(1);
+				i++;
+			}
+			j++;
+		}
+		free(game->map.line);
+	}
 	close(game->map.fd_map);
 }
