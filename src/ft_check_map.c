ace8ccf13805294e6e5621960e517e03c838e53a
Conradv2 <marek226@interia.pl>
2025-03-29 19:33:31 +0100

    added new functions for collectibles

--- src/ft_check_map.c
+++ src/ft_check_map.c
@@ -16,12 +16,74 @@
 // {
 
 // }
+void	ft_collectible_arr(t_game *game, int count)
+{
+	game->collectible = (t_entity *)malloc(sizeof(t_entity) * count);
+	if (game->collectible == NULL)
+	{
+		perror("ft_collectible_arr failed allocation");
+		ft_free_arr(game);
+		exit(1);
+	}
+}
+
+int	ft_collectible_counter(t_game *game)
+{
+	int	count;
+
+	count = 0;
+	game->map.i = 0;
+	while (game->map.i < game->map.row)
+	{
+		game->map.j = 0;
+		while (game->map.j < game->map.column)
+		{
+			if (game->map.map_arr[game->map.i][game->map.j] == 'C')
+			{
+				count++;
+			}
+			game->map.j++;
+		}
+		game->map.i++;
+	}
+	return (count);
+}
+
+int	ft_init_collectible(t_game *game)
+{
+	int	count;
+
+	count = ft_collectible_counter(game);
+	ft_collectible_arr(game, count);
+	game->map.i = 0;
+	while (game->map.i < game->map.row)
+	{
+		game->map.j = 0;
+		while (game->map.j < game->map.column)
+		{
+			if (game->map.map_arr[game->map.i][game->map.j] == 'C')
+			{
+				game->collectible[game->map.i].y = game->map.i;
+				game->collectible[game->map.j].x = game->map.j;
+			}
+			game->map.j++;
+		}
+		game->map.i++;
+	}
+	return (count);
+}
 
 void	ft_check_map(t_game *game)
 {
 	if (ft_init_player(game) != 1)
 	{
 		perror("There is too much or not a single player!...");
+		ft_free_arr(game);
+		exit(1);
+	}
+	if (!(ft_init_collectible(game) >= 1))
+	{
+		perror("There is too little collectibles to open the exit...");
 		ft_free_arr(game);
 		exit(1);
 	}
