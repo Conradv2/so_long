2eb0019f42e35d7295c6b1c4c13afdb277357582
Conradv2 <marek226@interia.pl>
2025-03-29 21:49:34 +0100

    first try at rendering a texture

--- src/ft_render_window.c
+++ src/ft_render_window.c
@@ -30,4 +30,10 @@ void	ft_render_window(t_game *window)
 		free(window->mlx);
 		exit(1);
 	}
+	window->wall.img.img = mlx_xpm_file_to_image(window->mlx, "./textures/Wall.xpm", &window->wall.img.width, &window->wall.img.height);
+	if (window->wall.img.img == NULL)
+	{
+		perror ("CHUJ");
+	}
+	mlx_put_image_to_window(window->mlx, window->mlx_win, window->wall.img.img, window->wall.img.width, window->wall.img.height);
 }
