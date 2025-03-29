ede1d4cdefb665e4825c791a6b92629e63d3b91b
Conradv2 <marek226@interia.pl>
2025-03-29 20:50:06 +0100

    added new exit check

--- src/ft_check_map.c
+++ src/ft_check_map.c
@@ -12,17 +12,44 @@
 
 #include "so_long.h"
 
+int	ft_init_exit(t_game *game)
+{
+	int	count;
+
+	count = 0;
+	game->map.i = 0;
+	while (game->map.i < game->map.row)
+	{
+		game->map.j = 0;
+		while (game->map.j < game->map.column)
+		{
+			if (game->map.map_arr[game->map.i][game->map.j] == 'E')
+				count++;
+			game->map.j++;
+		}
+		game->map.i++;
+	}
+	return (count);
+}
+
 void	ft_check_map(t_game *game)
 {
 	if (ft_init_player(game) != 1)
 	{
-		perror("There is too much or not a single player!...");
+		perror("There is too much or not a single player...");
 		ft_free_arr(game);
 		exit(1);
 	}
 	if (!(ft_init_collectible(game) >= 1))
 	{
 		perror("There is too little collectibles to open the exit...");
+		ft_free_arr(game);
+		free(game->collectible);
+		exit(1);
+	}
+	if (ft_init_exit(game) != 1)
+	{
+		perror("There is too much or not a single exit...");
 		ft_free_arr(game);
 		free(game->collectible);
 		exit(1);
