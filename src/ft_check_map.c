8589ec451df133ad5848190471ec3a46f375a380
Conrad <marek226@interia.pl>
2025-03-30 20:15:26 +0200

    shortening the function

--- src/ft_check_map.c
+++ src/ft_check_map.c
@@ -12,33 +12,33 @@
 
 #include "so_long.h"
 
+void	ft_fun(t_game *game)
+{
+	ft_free_arr(game);
+	free(game->collectible);
+	exit(1);
+}
+
 void	ft_check_map(t_game *game)
 {
-	if (ft_init_player(game) != 1)
-	{
-		perror("There is too much or not a single player...");
-		ft_free_arr(game);
-		exit(1);
-	}
 	if (!(ft_init_collectible(game) >= 1))
 	{
 		perror("There is too little collectibles to open the exit...");
-		ft_free_arr(game);
-		free(game->collectible);
-		exit(1);
+		ft_fun(game);
+	}
+	if (ft_init_player(game) != 1)
+	{
+		perror("There is too much or not a single player...");
+		ft_fun(game);
 	}
 	if (ft_init_exit(game) != 1)
 	{
 		perror("There is too much or not a single exit...");
-		ft_free_arr(game);
-		free(game->collectible);
-		exit(1);
+		ft_fun(game);
 	}
 	if (ft_wall_check(game) != 1)
 	{
 		perror("There is missing wall...");
-		ft_free_arr(game);
-		free(game->collectible);
-		exit(1);
+		ft_fun(game);
 	}
 }
