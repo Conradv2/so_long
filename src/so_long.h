5228270964795fa80ec031d47c53d5e08a0fa21b
Conrad <marek226@interia.pl>
2025-03-30 20:01:00 +0200

    added ft_wall_check declaration

--- src/so_long.h
+++ src/so_long.h
@@ -70,6 +70,7 @@ void	ft_check_map(t_game *game);
 int		ft_init_player(t_game *game);
 int		ft_init_collectible(t_game *game);
 int		ft_init_exit(t_game *game);
+int		ft_wall_check(t_game *game);
 void	ft_free_arr(t_game *game);
 int		ft_exit_game(t_game *game);
 int		ft_key_input(int keysym, t_game *game);
