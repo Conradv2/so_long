cc029bb179a60c36051688196f14fb7773e07d2d
Conradv2 <marek226@interia.pl>
2025-04-05 01:39:29 +0200

    minor changed that do nothing acutally

--- src/ft_handle_map.c
+++ src/ft_handle_map.c
@@ -16,8 +16,10 @@ void	ft_handle_map(t_game *game)
 {
 	ft_create_arr(game);
 	ft_print_arr(game);
-	ft_printf("\n");
+	// ft_printf("\n");
 	ft_check_map(game);
 	ft_printf("Exit position: y = [%d] x = [%d]\n", game->exit.y, game->exit.x);
+	ft_printf("Rows = %d\nColumns = %d\n", game->map.row, game->map.column);
+	ft_printf("The element = %d\n",game->map.map_arr[game->map.row - 1][game->map.column - 1]);
 	ft_print_arr(game);
 }
